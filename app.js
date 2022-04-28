a201031397a93d2e5971c2ebafd8df1a5c8f6cba
Benjoow <benjamin.wojeez@gmail.com>
2022-04-28 15:53:52 +0200

    modif js file and media queries

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const details = document.querySelectorAll(".accordion__content-line");
 
+const autoCloseAccordionItem = (event, target) => {
+  if (event.target.open) {
+    details.forEach( detail => {
+      if(target !== detail) {
+        detail.open = false;
+      }
+    })
+  }
+}
+
 details.forEach( target => {
   target.addEventListener("toggle", (event) => {
-    setTimeout(() => {
-      console.log(event.target.open);
-    }, 1000);
-    if(event.target.open) {
-      details.forEach( detail => {
-        if(target !== detail) {
-          detail.open = false;
-        }
-      })
-    }
+    autoCloseAccordionItem(event, target);
   })
 })
